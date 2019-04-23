ec417599f0cec36d54a0cc9b1ae5e2410dc1751b
David Ackerman <davidnackerman@gmail.com>
2019-04-22 20:44:35 -0700

    Feature 1 Complete.

--- Metrognome/app/src/main/java/com/example/metrognome/PlaybackActivity.java
+++ Metrognome/app/src/main/java/com/example/metrognome/PlaybackActivity.java
@@ -2,33 +2,29 @@ package com.example.metrognome;
 
 import android.media.AudioAttributes;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.media.SoundPool;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
-import android.os.Message;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
+/**
+ * An activity for handling playback of a metronome.
+ */
 public class PlaybackActivity extends AppCompatActivity {
     private static final float VOLUME = 1f;
+    private static final int MAX_SOUND_POOL_STREAMS = 20;
+    private static final long MILLIS_PER_BPM = 60000;
     private EditText editText;
     private Button startStopButton;
     private SoundPool soundPool;
     private int claveId;
     private Handler handler;
+    private MetronomeRunnable metronomeRunnable;
     private int beatsPerMinute;
     private boolean running;
 
@@ -47,29 +43,18 @@ public class PlaybackActivity extends AppCompatActivity {
     }
 
     private void init() {
-        editText = findViewById(R.id.bpm_edit_text);
-        startStopButton = findViewById(R.id.start_stop_button);
+        initEditText();
         initStartStopButton();
-        handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message msg) {
-                soundPool.play(1, VOLUME, VOLUME, 0, 0, 1f);
-                return true;
-            }
-        });
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
-                    .build();
-            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes).setMaxStreams(20).build();
-        } else {
-            soundPool = new SoundPool(20, AudioManager.STREAM_ALARM, 0);
-        }
-        claveId = soundPool.load(this, R.raw.clave1, 1);
+        initSoundPool();
+        initHandler();
+    }
+
+    private void initEditText() {
+        editText = findViewById(R.id.bpm_edit_text);
     }
 
     private void initStartStopButton() {
+        startStopButton = findViewById(R.id.start_stop_button);
         startStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,13 +72,28 @@ public class PlaybackActivity extends AppCompatActivity {
             }
         });
     }
+    
+    private void initSoundPool() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build();
+            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes).setMaxStreams(MAX_SOUND_POOL_STREAMS).build();
+        } else {
+            soundPool = new SoundPool(MAX_SOUND_POOL_STREAMS, AudioManager.STREAM_ALARM, 0);
+        }
+        claveId = soundPool.load(this, R.raw.clave1, 1);
+    }
+
+    private void initHandler() {
+        handler = new Handler();
+        metronomeRunnable = new MetronomeRunnable(handler, soundPool, claveId);
+    }
 
     private void startPlayer() {
-        long now = SystemClock.uptimeMillis();
-        long millisBetweenBeats = millisBetweenBeats();
-        for (int i = 0; i < 1000; i++) {
-            handler.sendMessageAtTime(handler.obtainMessage(), now + i * millisBetweenBeats);
-        }
+        metronomeRunnable.setInterval(millisBetweenBeats());
+        handler.post(metronomeRunnable);
     }
 
     private void stopPlayer() {
@@ -101,7 +101,7 @@ public class PlaybackActivity extends AppCompatActivity {
     }
 
     private long millisBetweenBeats() {
-        return 60 * 1000 / beatsPerMinute;
+        return MILLIS_PER_BPM / beatsPerMinute;
     }
 
     private boolean setBeatsPerMinute() {
@@ -118,5 +118,44 @@ public class PlaybackActivity extends AppCompatActivity {
             Toast.makeText(this, "Invalid BPM: Not an integer.", Toast.LENGTH_SHORT).show();
         }
         return false;
+    }
+
+    /**
+     * A runnable object that plays a sound at a specified interval. It invokes the handler and posts
+     * itself to run at the interval.
+     *
+     * It is expected to set the interval prior to calling run.
+     */
+    public class MetronomeRunnable implements Runnable {
+        private Handler handler;
+        private SoundPool soundPool;
+        private int soundId;
+        private long intervalInMillis = Long.MAX_VALUE;
+
+        /**
+         * Creates a new MetronomeRunnable
+         * @param handler the handler to post itself to,
+         * @param soundPool the SoundPool object to play,
+         * @param soundId the id of the audio to play.
+         */
+        public MetronomeRunnable(Handler handler, SoundPool soundPool, int soundId) {
+            this.handler = handler;
+            this.soundPool = soundPool;
+            this.soundId = soundId;
+        }
+
+        /**
+         * Sets the interval to repeat this sound at.
+         * @param interval the interval in milliseconds.
+         */
+        public void setInterval(long interval) {
+            intervalInMillis = interval;
+        }
+
+        @Override
+        public void run() {
+            soundPool.play(soundId, VOLUME, VOLUME, 0, 0, 1f);
+            handler.postDelayed(this, intervalInMillis);
+        }
     }
 }
