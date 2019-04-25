0b530473418e28d0e0eccc72df2b04a32d66f7ab
David Ackerman <davidnackerman@gmail.com>
2019-04-24 20:57:22 -0700

    Feature #2 First Draft

--- Metrognome/app/src/main/java/com/example/metrognome/PlaybackActivity.java
+++ Metrognome/app/src/main/java/com/example/metrognome/PlaybackActivity.java
@@ -1,32 +1,31 @@
 package com.example.metrognome;
 
-import android.media.AudioAttributes;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.example.metrognome.audio.SoundPoolWrapper;
+import com.example.metrognome.time.Measure;
+import com.example.metrognome.time.MeasureRunnable;
 
 /**
  * An activity for handling playback of a metronome.
  */
 public class PlaybackActivity extends AppCompatActivity {
-    private static final float VOLUME = 1f;
-    private static final int MAX_SOUND_POOL_STREAMS = 20;
-    private static final long MILLIS_PER_BPM = 60000;
     private EditText editText;
     private Button startStopButton;
-    private SoundPool soundPool;
-    private int claveId;
+    private SoundPoolWrapper soundPool;
     private Handler handler;
-    private MetronomeRunnable metronomeRunnable;
-    private int beatsPerMinute;
     private boolean running;
+    private Measure measure = new Measure();
+    private MeasureRunnable measureRunnable;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +36,12 @@ public class PlaybackActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-        soundPool.release();
-        soundPool = null;
         super.onDestroy();
     }
 
     private void init() {
-        initEditText();
-        initStartStopButton();
-        initSoundPool();
-        initHandler();
-    }
-
-    private void initEditText() {
         editText = findViewById(R.id.bpm_edit_text);
-    }
-
-    private void initStartStopButton() {
+        spinner = findViewById(R.id.spinner);
         startStopButton = findViewById(R.id.start_stop_button);
         startStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,44 +59,25 @@ public class PlaybackActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-    
-    private void initSoundPool() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .build();
-            soundPool = new SoundPool.Builder().setAudioAttributes(audioAttributes).setMaxStreams(MAX_SOUND_POOL_STREAMS).build();
-        } else {
-            soundPool = new SoundPool(MAX_SOUND_POOL_STREAMS, AudioManager.STREAM_ALARM, 0);
-        }
-        claveId = soundPool.load(this, R.raw.clave1, 1);
-    }
 
-    private void initHandler() {
+        soundPool = new SoundPoolWrapper(this);
         handler = new Handler();
-        metronomeRunnable = new MetronomeRunnable(handler, soundPool, claveId);
+        measureRunnable = new MeasureRunnable(measure, handler, soundPool);
     }
 
     private void startPlayer() {
-        metronomeRunnable.setInterval(millisBetweenBeats());
-        handler.post(metronomeRunnable);
+        handler.post(measureRunnable);
     }
 
     private void stopPlayer() {
         handler.removeCallbacksAndMessages(null);
     }
 
-    private long millisBetweenBeats() {
-        return MILLIS_PER_BPM / beatsPerMinute;
-    }
-
     private boolean setBeatsPerMinute() {
         try {
             int bpm = Integer.parseInt(editText.getText().toString());
             if (bpm >= 1 && bpm <= 600) {
-                beatsPerMinute = bpm;
+                measure.setTempo(bpm);
                 return true;
             } else {
                 Toast.makeText(this, "Invalid BPM: Integer must be between 1 and 600.", Toast.LENGTH_SHORT).show();
@@ -118,44 +87,5 @@ public class PlaybackActivity extends AppCompatActivity {
             Toast.makeText(this, "Invalid BPM: Not an integer.", Toast.LENGTH_SHORT).show();
         }
         return false;
-    }
-
-    /**
-     * A runnable object that plays a sound at a specified interval. It invokes the handler and posts
-     * itself to run at the interval.
-     *
-     * It is expected to set the interval prior to calling run.
-     */
-    public class MetronomeRunnable implements Runnable {
-        private Handler handler;
-        private SoundPool soundPool;
-        private int soundId;
-        private long intervalInMillis = Long.MAX_VALUE;
-
-        /**
-         * Creates a new MetronomeRunnable
-         * @param handler the handler to post itself to,
-         * @param soundPool the SoundPool object to play,
-         * @param soundId the id of the audio to play.
-         */
-        public MetronomeRunnable(Handler handler, SoundPool soundPool, int soundId) {
-            this.handler = handler;
-            this.soundPool = soundPool;
-            this.soundId = soundId;
-        }
-
-        /**
-         * Sets the interval to repeat this sound at.
-         * @param interval the interval in milliseconds.
-         */
-        public void setInterval(long interval) {
-            intervalInMillis = interval;
-        }
-
-        @Override
-        public void run() {
-            soundPool.play(soundId, VOLUME, VOLUME, 0, 0, 1f);
-            handler.postDelayed(this, intervalInMillis);
-        }
     }
 }
