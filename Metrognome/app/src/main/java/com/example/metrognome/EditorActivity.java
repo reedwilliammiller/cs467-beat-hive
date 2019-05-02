27a41897ca2055ded57e7b2ff4d1c63948930525
Adam Page <pagead@oregonstate.edu>
2019-05-01 19:30:47 -0700

    Editor recycler view basic function
    The RecyclerView object is up, works, and I can manipulate individual objects within the list its passed. Next is to take what is passed and populate the actual beat view.

--- Metrognome/app/src/main/java/com/example/metrognome/EditorActivity.java
+++ Metrognome/app/src/main/java/com/example/metrognome/EditorActivity.java
@@ -4,21 +4,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import com.example.metrognome.editor.EditorAdapter;
+import com.example.metrognome.time.Beat;
 
 public class EditorActivity extends AppCompatActivity {
+
+    RecyclerView rv;
+    RecyclerView.LayoutManager lm;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         init();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {}
     }
 
     private void init() {
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
-                LinearLayoutManager.HORIZONTAL, false);
-
-        RecyclerView editorBeats = findViewById(R.id.recycler);
-        editorBeats.setLayoutManager(layoutManager);
+        rv = findViewById(R.id.recycler);
+        lm = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rv.setLayoutManager(lm);
+        adapter = new EditorAdapter(this, new Beat[]{new Beat(), new Beat()});
+        rv.setAdapter(adapter);
     }
 }
