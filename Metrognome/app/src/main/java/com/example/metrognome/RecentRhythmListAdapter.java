87f0a1f6cdc25b11f8ddae652a0a568dcb394337
Reed Miller <millreed@oregonstate.edu>
2019-05-04 15:10:51 -0700

    added database

---
+++ Metrognome/app/src/main/java/com/example/metrognome/RecentRhythmListAdapter.java
@@ -0,0 +1,56 @@
+package com.example.metrognome;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.List;
+
+
+public class RecentRhythmListAdapter extends RecyclerView.Adapter<RecentRhythmListAdapter.RhythmViewHolder> {
+    class RhythmViewHolder extends RecyclerView.ViewHolder {
+        private final Button rhythmItemView;
+
+        private RhythmViewHolder(View itemView) {
+            super(itemView);
+            rhythmItemView = itemView.findViewById(R.id.rhythmOpenButton);
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Rhythm> mRhythms;
+
+    RecentRhythmListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+    @Override
+    public RhythmViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_recent_rhythm_file, parent, false);
+        return new RhythmViewHolder(itemView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RhythmViewHolder holder, int position) {
+        if (mRhythms != null) {
+            Rhythm current = mRhythms.get(position);
+            holder.rhythmItemView.setText(current.getTitle());
+        } else {
+            holder.rhythmItemView.setText("No Rhythm");
+        }
+    }
+
+    void setRhythms(List<Rhythm> rhythms) {
+        mRhythms = rhythms;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mRhythms != null)
+            return mRhythms.size();
+        else return 0;
+    }
+}
