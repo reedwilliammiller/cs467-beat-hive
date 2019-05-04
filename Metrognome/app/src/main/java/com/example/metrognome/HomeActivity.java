687545ff0ed341020a94c78c210cb4ba630e3e11
Reed Miller <millreed@oregonstate.edu>
2019-05-04 15:10:51 -0700

    added database

--- Metrognome/app/src/main/java/com/example/metrognome/HomeActivity.java
+++ Metrognome/app/src/main/java/com/example/metrognome/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.metrognome;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity{
@@ -11,6 +15,21 @@ public class HomeActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerviewrecent);
+        final RecentRhythmListAdapter adapter = new RecentRhythmListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mRecentRhythmViewModel = ViewModelProviders.of(this).get(RecentRhythmViewModel.class);
+
+        mRecentRhythmViewModel.getRecentRhythms().observe(this, new Observer<List<Rhythm>>() {
+            @Override
+            public void onChanged(@Nullable final List<Rhythm> rhythms) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setRhythms(rhythms);
+            }
+        });
     }
 
     public void startPlayback(View view) {
