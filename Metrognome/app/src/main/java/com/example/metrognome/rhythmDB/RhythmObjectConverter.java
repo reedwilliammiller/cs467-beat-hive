56930e5d52dc143be2fb786a44f9da35cb4e6182
Reed Miller <millreed@oregonstate.edu>
2019-05-11 13:55:30 -0700

    RhythmObjectConverter has issues

--- Metrognome/app/src/main/java/com/example/metrognome/rhythmDB/RhythmObjectConverter.java
+++ Metrognome/app/src/main/java/com/example/metrognome/rhythmDB/RhythmObjectConverter.java
@@ -1,17 +1,14 @@
 package com.example.metrognome.rhythmDB;
 
 import android.arch.persistence.room.TypeConverter;
-import android.os.Build;
 
 import com.example.metrognome.time.Rhythm;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.Base64;
 
 public class RhythmObjectConverter {
     @TypeConverter
@@ -20,18 +17,20 @@ public class RhythmObjectConverter {
             return null;
         }
         else {
-            final byte[] bytes;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                System.out.println("De-serializing");
-                bytes = Base64.getDecoder().decode(value);
-                try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes); ObjectInput in = new ObjectInputStream(bis)) {
-                    return (Rhythm) in.readObject();
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                    return null;
-                }
+            try {
+                byte serialized[] = value.getBytes();
+                ByteArrayInputStream in = new ByteArrayInputStream(serialized);
+                ObjectInputStream oin = null;
+                oin = new ObjectInputStream(in);
+                Rhythm rhythm = (Rhythm) oin.readObject();
+                return rhythm;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                return null;
             }
-            else return null;
         }
     }
 
@@ -42,16 +41,14 @@ public class RhythmObjectConverter {
         } else {
             String serializedObject;
             try {
-                System.out.println("serializing");
+                System.out.println("serializing!!");
                 ByteArrayOutputStream out = new ByteArrayOutputStream();
                 ObjectOutputStream ostr = new ObjectOutputStream(out);
                 ostr.writeObject(rhythm);
                 ostr.flush();
-                final byte[] byteArray = out.toByteArray();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    return Base64.getEncoder().encodeToString(byteArray);
-                }
-                else return null;
+                serializedObject = out.toString();
+                System.out.println(serializedObject);
+                return serializedObject;
             } catch (Exception e) {
                 System.out.println(e);
                 return null;
